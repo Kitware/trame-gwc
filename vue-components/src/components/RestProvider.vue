63d54c6f5fcbf87ec42a31aa0fc93bcd57a17af9
Justine Antoine <justine.antoine@kitware.com>
2024-11-27 11:22:03 +0100

    fix(vue-components): fix build and linter issues

--- vue-components/src/components/RestProvider.vue
+++ vue-components/src/components/RestProvider.vue
@@ -1,54 +1,56 @@
 <template>
+  <div>
     <slot></slot>
+  </div>
 </template>
 
 <script>
-    import { RestClient, NotificationBus } from '@girder/components/src';
-    import { computed } from "vue";
-    import { vuetifyConfig } from '@girder/components/src/utils'
+import { RestClient, NotificationBus } from '@girder/components/src';
+import { computed } from 'vue';
+import { vuetifyConfig } from '@girder/components/src/utils';
 
-    export default {
-        inject: ['trame'],
-        data() {
-            return {
-                girderRest: null,
-                notificationBus: null
-            };
-        },
-        async created() {
-            // Register Girder Web Component icons mapping in Vuetify
-            Object.assign(this.$vuetify.icons.values, vuetifyConfig.icons.values);
-
-            this.girderRest = new RestClient({ apiRoot: this.trame.state.get("api_root") });
-            this.notificationBus = new NotificationBus(
-                this.girderRest, {useEventSource: true,});
-
-            const user = await this.girderRest.fetchUser()
-            if (user){
-                this.trame.state.set("user", user)
-                this.notificationBus.connect()
-            }
-
-            this.girderRest.$on("login", () => { this.login() })
-        },
-        provide() {
-            return {
-                girderRest: computed(() => this.girderRest),
-                notificationBus: computed(() => this.notificationBus)
-            }
-        },
-        methods:
-        {   
-            async login(){
-                const user = await this.girderRest.fetchUser()
-                if (user){
-                    this.trame.state.set("user", user)
-                }
-            },
-            logout(){
-                this.girderRest.logout()
-                this.trame.state.set("user", null)
-            }
-        }
+export default {
+  inject: ['trame'],
+  data() {
+    return {
+      girderRest: null,
+      notificationBus: null,
     };
+  },
+  async created() {
+    // Register Girder Web Component icons mapping in Vuetify
+    Object.assign(this.$vuetify.icons.values, vuetifyConfig.icons.values);
+
+    this.girderRest = new RestClient({ apiRoot: this.trame.state.get('api_root') });
+    this.notificationBus = new NotificationBus(this.girderRest, { useEventSource: true });
+
+    const user = await this.girderRest.fetchUser();
+    if (user) {
+      this.trame.state.set('user', user);
+      this.notificationBus.connect();
+    }
+
+    this.girderRest.$on('login', () => {
+      this.login();
+    });
+  },
+  provide() {
+    return {
+      girderRest: computed(() => this.girderRest),
+      notificationBus: computed(() => this.notificationBus),
+    };
+  },
+  methods: {
+    async login() {
+      const user = await this.girderRest.fetchUser();
+      if (user) {
+        this.trame.state.set('user', user);
+      }
+    },
+    logout() {
+      this.girderRest.logout();
+      this.trame.state.set('user', null);
+    },
+  },
+};
 </script>
--- vue-components/vue.config.js
+++ vue-components/vue.config.js
@@ -2,7 +2,5 @@ const path = require('path');
 const DST_PATH = '../trame_gwc/module/serve';
 
 module.exports = {
-    lintOnSave: false,
-    outputDir: path.resolve(__dirname, DST_PATH),
-    publicPath: './'
+  outputDir: path.resolve(__dirname, DST_PATH),
 };
